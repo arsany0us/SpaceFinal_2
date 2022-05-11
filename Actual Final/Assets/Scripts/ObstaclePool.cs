2dbacbefc5ee48b4975a2abb98ea9b3a94c49a47
Galindo <timothygalindo07@gmail.com>
2022-05-11 10:56:36 -0700

    add animation and scripts

---
+++ Actual Final/Assets/Scripts/ObstaclePool.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class ObstaclePool : MonoBehaviour
+{
+    public GameObject columnPrefab;
+    public int columnPoolSize = 5;
+    public float spawnRate = 2f;
+    public float enemyMin = 0f;
+    public float enemyMax = 0f;                                    //Maximum y value of the column position.
+
+    private GameObject[] columns;                                    //Collection of pooled columns.
+    private int currentColumn = 0;                                    //Index of the current column in the collection.
+
+    private Vector2 objectPoolPosition = new Vector2(-15, -25);        //A holding position for our unused columns offscreen.
+    private float spawnXPosition = 10f;
+
+    private float timeSinceLastSpawned;
+
+
+    void Start()
+    {
+        timeSinceLastSpawned = 0f;
+
+
+        columns = new GameObject[columnPoolSize];
+
+        for (int i = 0; i < columnPoolSize; i++)
+        {
+
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+
+
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0f;
+
+
+            float spawnYPosition = Random.Range(enemyMin, enemyMax);
+
+
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+
+
+            currentColumn++;
+
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
+    }
+}
---
+++ Actual Final/Assets/Scripts/ScrollingObjects.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObjects : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        rb2d = GetComponent<Rigidbody2D>();
+             
+        rb2d.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
+    }
+
+    void Update()
+    {
+        if (GameControl.instance.gameOver == true)
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+    }
+}
